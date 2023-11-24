03f45169b885faa2e44799eaaa3a7eb497b84dbc
yuling <gb2013429@gmail.com>
2023-11-24 20:27:24 +0800

    Updates

--- assets/index-46da3a02.js
+++
@@ -1,1 +0,0 @@
-import"./main-2efb94f1.js";new Swiper(".mySwiper",{autoplay:{delay:5e3},mousewheel:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"}});

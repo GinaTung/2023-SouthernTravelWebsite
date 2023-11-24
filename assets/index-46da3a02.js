bc848c811f065d334f1b232132ed6fb4cf335879
yuling <gb2013429@gmail.com>
2023-11-24 20:07:17 +0800

    Updates

---
+++ assets/index-46da3a02.js
@@ -0,0 +1,1 @@
+import"./main-2efb94f1.js";new Swiper(".mySwiper",{autoplay:{delay:5e3},mousewheel:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"}});
--- assets/index-99eb53c2.js
+++
@@ -1,1 +0,0 @@
-import"./main-941adb1a.js";new Swiper(".mySwiper",{autoplay:{delay:5e3},mousewheel:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"}});

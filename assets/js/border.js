e2f88635574205ed57e97ee67c9ed1284f4086ac
yuling <gb2013429@gmail.com>
2023-11-26 14:47:16 +0800

    update rwd + radius js

---
+++ assets/js/border.js
@@ -0,0 +1,21 @@
+var navbarToggle = document.querySelector(".navbar-toggler");
+var navbarBorder = document.querySelector(".border-double-bottom");
+
+navbarToggle.addEventListener("click", function () {
+  // 切換樣式
+  navbarBorder.classList.toggle("rounded-bottom");
+});
+
+
+
+
+
+
+let sum = 0;
+/* 程式碼撰寫 */
+const plusOne = document.querySelector(".plusOne");
+plusOne.addEventListener("click",function(e){
+  sum+=1;
+plusOne.textContent = `＋${sum}`;
+  console.log(sum);
+})

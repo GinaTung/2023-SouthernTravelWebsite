3ac0278cf85f68b519e718ec466fb606d6e7c021
yuling <gb2013429@gmail.com>
2023-12-11 21:57:31 +0800

    updata+ change heart + api js

---
+++ assets/js-json/config.js
@@ -0,0 +1,6 @@
+// const url = "https://tourist-web-2023js-1.onrender.com";
+// let id = "";
+export const api_url = 'http://localhost:3000';
+export let token = localStorage.getItem("token");
+
+// token = localStorage.getItem("token")
